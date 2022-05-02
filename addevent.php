01ea8450c6696ecbbe65608bdfc1a087a369ad0b
alexisvafiadis <104735242+alexisvafiadis@users.noreply.github.com>
2022-05-02 23:07:46 +0100

    Add files via upload

---
+++ addevent.php
@@ -0,0 +1,30 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<form method="post" action="" name="addevent-form">
+  <div class="form-element">
+    <label>Name</label>
+    <input type="text" name="eventname" required />
+  </div>
+  <div class="form-element">
+    <label>Description</label>
+    <input type="text" name="eventdesc" required />
+  </div>
+  <div class="form-element">
+    <label>Date</label>
+    <input type="date" name="eventdate" required />
+  </div>
+  <div class="form-element">
+    <label>Time</label>
+    <input type="time" name="eventtime" required />
+  </div>
+  <div class="form-element">
+    <label>Description</label>
+    <input type="" name="eventtime" required />
+  </div>
+  <div class="form-element">
+    <label>Organizers</label>
+    <input type="text" name="eventorganizers" required />
+  </div>
+  <button type="submit" name="addevent" value="addevent">Add Event</button>
+</form>
+</html>
---
+++ panel.php
@@ -0,0 +1,80 @@
+<?php
+include("config.php");
+$db= $connection;
+$tableName="user";
+$columns= ['id', 'email', 'firstname','lastname','subscribed'];
+$fetchData = fetch_data($db, $tableName, $columns);
+function fetch_data($db, $tableName, $columns){
+ if(empty($db)){
+  $msg= "Database connection error";
+ }elseif (empty($columns) || !is_array($columns)) {
+  $msg="columns Name must be defined in an indexed array";
+ }elseif(empty($tableName)){
+   $msg= "Table Name is empty";
+}else{
+$columnName = implode(", ", $columns);
+$query = "SELECT ".$columnName." FROM ".$tableName." ORDER BY id DESC";
+$result = $db->query($query);
+if($result== true){ 
+ if ($result->num_rows > 0) {
+    $row= mysqli_fetch_all($result, MYSQLI_ASSOC);
+    $msg= $row;
+ } else {
+    $msg= "No Data Found"; 
+ }
+}else{
+  $msg= mysqli_error($db);
+}
+}
+return $msg;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+ <div class="row">
+   <div class="col-sm-8">
+    <?php echo $deleteMsg??''; ?>
+    <div class="table-responsive">
+      <table class="table table-bordered">
+       <thead><tr><th>ID</th>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>Email</th>
+         <th>Subscribed</th>
+    </thead>
+    <tbody>
+  <?php
+      if(is_array($fetchData)){      
+      $sn=1;
+      foreach($fetchData as $data){
+    ?>
+      <tr>
+      <td><?php echo $sn; ?></td>
+      <td><?php echo $data['firstname']??''; ?></td>
+      <td><?php echo $data['lastname']??''; ?></td>
+      <td><?php echo $data['email']??''; ?></td>
+      <td><?php echo $data['subscribed']??''; ?></td>
+     </tr>
+     <?php
+      $sn++;}}else{ ?>
+      <tr>
+        <td colspan="8">
+    <?php echo $fetchData; ?>
+  </td>
+    <tr>
+    <?php
+    }?>
+    </tbody>
+     </table>
+   </div>
+</div>
+</div>
+</div>
+</body>
+</html>
